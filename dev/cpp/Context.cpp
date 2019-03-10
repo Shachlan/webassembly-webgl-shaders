2be8476a628396b7b2b3344ac1336a77117c898a
Shachar Langbeheim <shachar@lightricks.com>
2019-03-10 09:52:20 +0200

    temp

--- dev/cpp/Context.cpp
+++ dev/cpp/Context.cpp
@@ -16,23 +16,10 @@ std::string mix_images_fragment_source =
     "varying vec2 v_texCoord;                            \n"
     "uniform sampler2D texture1;                        \n"
     "uniform sampler2D texture2;                        \n"
-    "void main()                                         \n"
-    "{        \n"
-    "   texture1;\n"                                      
-    "  vec4 color1 = texture2D(texture1, v_texCoord) * 0.5;    \n"
-    "  vec4 color2 = texture2D(texture2, v_texCoord) * 0.5;    \n"                                 
-    "  gl_FragColor = color1 + color2;   \n"
-    "}                                                   \n";
-
-
-std::string edge_detect_fragment_source =
-    "precision mediump float;                            \n"
-    "varying vec2 v_texCoord;                            \n"
-    "uniform sampler2D texture1;                        \n"
     "uniform float width;  \n"
     "uniform float height;  \n"
     "void main()                                         \n"
-    "{          \n"
+    "{        \n"
     "  vec4 pixel = texture2D(texture1, v_texCoord);              \n"
     "  vec4 n[9];\n"
 
@@ -63,7 +50,45 @@ std::string edge_detect_fragment_source =
     "  sobel_y.b = avg_y;\n"
 
     "  vec3 sobel = vec3(sqrt((sobel_x.rgb * sobel_x.rgb) + (sobel_y.rgb * sobel_y.rgb)));\n"
-    "  gl_FragColor = vec4( sobel, 1.0 );   \n"
+    "  vec4 color1 = vec4( sobel, 1.0 ) * 0.5;   \n"  
+
+    "  pixel = texture2D(texture2, v_texCoord);              \n"
+    "  n[0] = texture2D(texture2, v_texCoord + vec2(0.0, 0.0) );\n"
+    "  n[1] = texture2D(texture2, v_texCoord + vec2(w, 0.0) );\n"
+    "  n[2] = texture2D(texture2, v_texCoord + vec2(2.0*w, 0.0) );\n"
+    "  n[3] = texture2D(texture2, v_texCoord + vec2(0.0*w, h) );\n"
+    "  n[4] = texture2D(texture2, v_texCoord + vec2(w, h) );\n"
+    "  n[5] = texture2D(texture2, v_texCoord + vec2(2.0*w, h) );\n"
+    "  n[6] = texture2D(texture2, v_texCoord + vec2(0.0, 2.0*h) );\n"
+    "  n[7] = texture2D(texture2, v_texCoord + vec2(w, 2.0*h) );\n"
+    "  n[8] = texture2D(texture2, v_texCoord + vec2(2.0*w, 2.0*h) );\n"
+
+    "  sobel_x = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);\n"
+    "  sobel_y = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);\n"
+
+    "  avg_x = (sobel_x.r + sobel_x.g + sobel_x.b) / 3.0;\n"
+    "  avg_y = (sobel_y.r + sobel_y.g + sobel_y.b) / 3.0;\n"
+
+    "  sobel_x.r = avg_x;\n"
+    "  sobel_x.g = avg_x;\n"
+    "  sobel_x.b = avg_x;\n"
+    "  sobel_y.r = avg_y;\n"
+    "  sobel_y.g = avg_y;\n"
+    "  sobel_y.b = avg_y;\n"
+
+    "  sobel = vec3(sqrt((sobel_x.rgb * sobel_x.rgb) + (sobel_y.rgb * sobel_y.rgb)));\n"
+    "  vec4 color2 = vec4( sobel, 1.0 ) * 0.5;   \n"                             
+    "  gl_FragColor = color1 + color2;   \n"
+    "}                                                   \n";
+
+
+std::string edge_detect_fragment_source =
+    "precision mediump float;                            \n"
+    "varying vec2 v_texCoord;                            \n"
+    "uniform sampler2D texture1;                        \n"
+    "void main()                                         \n"
+    "{          \n"
+
     "}                                                   \n";
 
 
@@ -96,7 +121,6 @@ GLuint CompileShader (GLenum type, std::string *source) {
 
         // And print them out
         glGetShaderInfoLog( shader, infoLen, NULL, infoLog );
-        printf("%s\n", infoLog);
 
         free ( infoLog );
     }
