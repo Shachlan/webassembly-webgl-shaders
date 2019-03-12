e3d5871d42b46ee0d612d9a2e9a94449186ecb6b
Shachar Langbeheim <shachar@lightricks.com>
2019-03-12 09:34:14 +0200

    temp

--- dev/cpp/Context.cpp
+++ dev/cpp/Context.cpp
@@ -15,7 +15,6 @@ std::string mix_images_fragment_source =
     "precision mediump float;                            \n"
     "varying vec2 v_texCoord;                            \n"
     "uniform sampler2D texture1;                        \n"
-    "uniform sampler2D texture2;                        \n"
     "uniform float width;  \n"
     "uniform float height;  \n"
     "void main()                                         \n"
@@ -50,35 +49,7 @@ std::string mix_images_fragment_source =
     "  sobel_y.b = avg_y;\n"
 
     "  vec3 sobel = vec3(sqrt((sobel_x.rgb * sobel_x.rgb) + (sobel_y.rgb * sobel_y.rgb)));\n"
-    "  vec4 color1 = vec4( sobel, 1.0 ) * 0.5;   \n"  
-
-    "  pixel = texture2D(texture2, v_texCoord);              \n"
-    "  n[0] = texture2D(texture2, v_texCoord + vec2(0.0, 0.0) );\n"
-    "  n[1] = texture2D(texture2, v_texCoord + vec2(w, 0.0) );\n"
-    "  n[2] = texture2D(texture2, v_texCoord + vec2(2.0*w, 0.0) );\n"
-    "  n[3] = texture2D(texture2, v_texCoord + vec2(0.0*w, h) );\n"
-    "  n[4] = texture2D(texture2, v_texCoord + vec2(w, h) );\n"
-    "  n[5] = texture2D(texture2, v_texCoord + vec2(2.0*w, h) );\n"
-    "  n[6] = texture2D(texture2, v_texCoord + vec2(0.0, 2.0*h) );\n"
-    "  n[7] = texture2D(texture2, v_texCoord + vec2(w, 2.0*h) );\n"
-    "  n[8] = texture2D(texture2, v_texCoord + vec2(2.0*w, 2.0*h) );\n"
-
-    "  sobel_x = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);\n"
-    "  sobel_y = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);\n"
-
-    "  avg_x = (sobel_x.r + sobel_x.g + sobel_x.b) / 3.0;\n"
-    "  avg_y = (sobel_y.r + sobel_y.g + sobel_y.b) / 3.0;\n"
-
-    "  sobel_x.r = avg_x;\n"
-    "  sobel_x.g = avg_x;\n"
-    "  sobel_x.b = avg_x;\n"
-    "  sobel_y.r = avg_y;\n"
-    "  sobel_y.g = avg_y;\n"
-    "  sobel_y.b = avg_y;\n"
-
-    "  sobel = vec3(sqrt((sobel_x.rgb * sobel_x.rgb) + (sobel_y.rgb * sobel_y.rgb)));\n"
-    "  vec4 color2 = vec4( sobel, 1.0 ) * 0.5;   \n"                             
-    "  gl_FragColor = color1 + color2;   \n"
+    "  gl_FragColor = vec4( sobel, 1.0 );   \n"  
     "}                                                   \n";
 
 GLuint CompileShader (GLenum type, std::string *source) {
@@ -122,12 +93,10 @@ void Context::setupTexture(const GLchar * name, GLenum texture, GLuint texId, in
     glBindTexture(GL_TEXTURE_2D, texId);
 }
 
-Context::Context (int w, int h, char * id, GLuint texture1, GLuint texture2) {
-
+Context::Context (int w, int h, char * id, GLuint texture1) {
     width = w;
     height = h;
     textureLoc1 = texture1;
-    textureLoc2 = texture2;
 
     // Context configurations
     EmscriptenWebGLContextAttributes attrs;
@@ -135,7 +104,7 @@ Context::Context (int w, int h, char * id, GLuint texture1, GLuint texture2) {
     attrs.depth = 1;
     attrs.stencil = 1;
     attrs.antialias = 1;
-    attrs.majorVersion = 2;
+    attrs.majorVersion = 1;
     attrs.minorVersion = 0;
 
     context = emscripten_webgl_create_context(id, &attrs);
@@ -161,8 +130,7 @@ Context::Context (int w, int h, char * id, GLuint texture1, GLuint texture2) {
     float heightUniform = glGetUniformLocation(programObject, "height");
     glUniform1f(widthUniform, (float) width);
     glUniform1f(heightUniform, (float) height);
-    setupTexture("texture1", GL_TEXTURE0, textureLoc1, 0);
-    setupTexture("texture2", GL_TEXTURE1, textureLoc2, 1);    
+    setupTexture("texture1", GL_TEXTURE0, textureLoc1, 0); 
     GLuint vertexObject;
     GLuint indexObject;
 
--- index.js
+++ index.js
@@ -14,15 +14,13 @@ window.addEventListener("wasmLoaded", () => {
 
   /** @type {HTMLVideoElement} */
   const firstVideoElement = document.createElement("video");
-  /** @type {HTMLVideoElement} */
-  const secondVideoElement = document.createElement("video");
   const canvas = document.getElementById("renderTarget");
   canvas.width = 1920;
   canvas.height = 1080;
   const convert = document.getElementById("convert");
 
-  function createCanvas(textureId1, textureId2) {
-    createContext(canvas, 0, textureId1, textureId2);
+  function createCanvas(textureId1) {
+    createContext(canvas, 0, textureId1);
   }
 
   function loadFirstVideo(src) {
@@ -30,31 +28,20 @@ window.addEventListener("wasmLoaded", () => {
     firstVideoElement.src = src;
   }
 
-  function loadSecondVideo(src) {
-    secondVideoElement.src = src;
-  }
-
   loadFirstVideo("./dog.mp4");
-  loadSecondVideo("./race.mp4");
 
   convert.addEventListener("click", () => {
-    const context = canvas.getContext("webgl2");
+    const context = canvas.getContext("webgl");
 
     const texture1 = initTexture(context);
-    const texture2 = initTexture(context);
     const textureId1 = RegisterNativeTextureId(texture1);
-    const textureId2 = RegisterNativeTextureId(texture2);
-    createCanvas(textureId1, textureId2);
+    createCanvas(textureId1);
 
-    const frameRenderer = createFrameRenderer(60);
-    frameRenderer.render(() => {
+    createFrameRenderer(30).render(() => {
       updateTexture(context, texture1, firstVideoElement);
-      updateTexture(context, texture2, secondVideoElement);
       renderFrame();
     });
 
-    startVideoStream([firstVideoElement, secondVideoElement]);
-    firstVideoElement.onended = frameRenderer.stop;
-    secondVideoElement.onended = frameRenderer.stop;
+    startVideoStream([firstVideoElement]);
   });
 });
