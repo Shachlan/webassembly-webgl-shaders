d8c9eebdd1857a966449dc8ffca65224705d3aab
Shachar Langbeheim <shachar@lightricks.com>
2019-05-14 12:10:17 +0300

    wasm loading into arrays.

--- videoRenderer.js
+++ videoRenderer.js
@@ -1,4 +1,5 @@
 import { ccallArrays } from "./wasm-arrays";
+import { size as globalSize } from "./globals";
 
 /**
  *
@@ -21,11 +22,26 @@ export function clearContex() {
   Module.ccall("clearContexts", null, null, null);
 }
 
-/**
- *
- * @param {Uint8Array} firstImageData
- * @param {ImageData} secondImageData
- */
-export function renderFrame(textureId1, textureId2) {
-  Module.ccall("invertFrameRun", null, ["number"], [textureId1]);
+export function renderFrame(
+  ctx,
+  buffer1,
+  buffer2,
+  result_buffer,
+  result_array
+) {
+  Module.ccall(
+    "invertFrameRun",
+    null,
+    ["number", "number"],
+    [buffer1, result_buffer]
+  );
+
+  result_array.fill(128);
+
+  for (let i = 0; i < result_array.length; i++) {
+    result_array.set([Module.HEAPU8[result + i]], i);
+  }
+
+  const img = new ImageData(result_array, globalSize.width, globalSize.height);
+  ctx.putImageData(img, 0, 0);
 }
